cd4f20829c4110dc64cd78b88b835fde9ba3fffe
Christian Leweling <c.leweling@gmail.com>
2018-07-08 11:25:30 +0000

    third edit

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect("mongodb://cleweling:Badmogihouse3@ds131601.mlab.com:31601/todo_prod");
+mongoose.connect("mongodb://cleweling:pupsis3@ds131601.mlab.com:31601/todo_prod");
 
 //mongoose.connect('mongodb://localhost/todos-api');
 
